0e91b59913d43c881e95a0afd62204b27c2e86a5
asenadesarrollo <asenadesarrollo@ccercafe.com.co>
2018-11-19 14:10:53 -0500

    segundo commit

--- products/models.py
+++ products/models.py
@@ -1,48 +1,44 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from utilities.models import *
 from django.db import models
+import uuid
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Countries(models.Model):
-    id_contry = models.AutoField(primary_key=True)
-    country_name = models.CharField(max_length=250)
-    def __str__(self): 
-        return self.country_name
-
-class States(models.Model):
-    id_state = models.AutoField(primary_key=True)
-    country = models.ForeignKey(Countries)
-    state = models.CharField(max_length=255)
+class UnitMeasurement(models.Model): 
+    id_measure = models.AutoField(primary_key=True)
+    name_measure = models.CharField(max_length=40)
     def __str__(self):
-        return self.state
+        return self.name_measure
 
-class TypeIdentification(models.Model):
-    id_type_identification = models.AutoField(primary_key=True)
-    concept = models.CharField(max_length=250) 
-
+class Category(models.Model):
+    id_category = models.AutoField(primary_key=True)
+    name_category = models.CharField(max_length=250)
+    sector_id = models.ForeignKey(Sectors)
     def __str__(self):
-		return self.concept
+        return self.name_category
 
-class ContributeRegimen(models.Model):
-    id_contribute_regimen = models.AutoField(primary_key=True)
-    regimen = models.CharField(max_length=250)
-
+class Brand(models.Model):
+    id_brand = models.AutoField(primary_key=True)
+    name_brand = models.CharField(max_length=250)
+    description_brand = models.TextField()
+    logo = models.ImageField(upload_to="media/logos")
     def __str__(self):
-		return self.regimen
+        return self.name_brand
 
-class Rules(models.Model):
-    id_rules = models.AutoField(primary_key=True)
-    rule = models.CharField(max_length=250)
-
+class Product(models.Model):
+    id_product = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name_product = models.CharField(max_length=200)
+    description = models.TextField()
+    category_id = models.ForeignKey(Category)
+    brand_id = models.ForeignKey(Brand)
+    measure_id = models.ForeignKey(UnitMeasurement)
+    provider_id = models.ForeignKey(User)
+    sector_id = models.ForeignKey(Sectors)
+    ranking = models.DecimalField(max_digits=3, decimal_places=2)
     def __str__(self):
-		return self.rule
+        return self.name_product
 
-class Sectors(models.Model):
-    id_sector = models.AutoField(primary_key=True)
-    name_sector = models.CharField(max_length=250)
-    description_sector = models.TextField()
-    
-    def __str__(self):
-        return self.name_sector
-
+class ImagesProduct(models.Model):
+    product_id = models.ForeignKey(Product, related_name='images_products')
+    image = models.ImageField(upload_to="media/images_products")
